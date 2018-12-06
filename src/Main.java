e58cce6dcff1d80b27e41e645627ded48c9210dd
Kasper <kasperdi1412@gmail.com>
2018-12-06 20:49:19 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ShortPath.testAll();
+        //ShortPath.test9();
+        //ShortPath.test10();
+        //ShortPath.test13();
+    }
+}
